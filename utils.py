2114cdc4a4f4aa662bd501dbd917b5705bb2fd1b
Isuru Maduranga <isurumaduranga.16@cse.mrt.ac.lk>
2021-02-04 00:59:27 +0530

    implement query_builder function

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-import socket
+from exceptions import MessageLengthError,ResponseError,InvalidQueryError
 from constants import HEADER_LENGTH,BUFFER_SIZE,RESPONSE_CODES
-from exceptions import MessageLengthError
+import socket
 
 def query_builder(type,data):
 
@@ -14,8 +14,22 @@ def query_builder(type,data):
     return bytes(query,"utf-8")
 
 def query_parser(query):
-    # to be implemented
-    pass
+
+    query = query.split(" ")
+    res_type = query[0]
+    res_code = query[1]
+
+    if res_type in RESPONSE_CODES and res_code in RESPONSE_CODES[res_type]:
+        
+        code = RESPONSE_CODES[res_type][res_code]
+
+        if code["stat"]:
+            return query[0],query[2:]
+        else:
+            raise ResponseError(code["msg"])
+    else:
+        raise InvalidQueryError
+
 
 def udp_recv(sock):
 
@@ -33,7 +47,9 @@ def udp_send_recv(ip,port,data,recieve=True):
     if recieve:
         return udp_recv(s)
 
+
 if __name__ == "__main__":
 
     # quick unit tests
-    pass
+    print(query_builder("REG",["129.82.123.45","5001","1234abcd"]).decode("utf-8"))
+    print(query_builder("REGOK",["2","129.82.123.45","5001","64.12.123.190","34001"]).decode("utf-8"))
