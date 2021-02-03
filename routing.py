13c918ab19b80d2b2c7cc67cd6c4311584ee154b
Isuru Maduranga <isurumaduranga.16@cse.mrt.ac.lk>
2021-02-04 01:01:19 +0530

    make RoutingTable class Singleton

--- routing.py
+++ routing.py
@@ -1,6 +1,16 @@
 
-class RoutingTable():
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class RoutingTable(metaclass=Singleton):
 
     def __init__(self): 
         self.table = []
+
+    def add(self,ip,port):
+        self.table.append((ip,port))
     
