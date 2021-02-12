49a5dd025eb0c1375d54db97864acf49e5687b25
ravin <ravinsg@gmail.com>
2021-02-12 18:10:19 +0530

    Implement the file searching functionality.

--- request_handler.py
+++ request_handler.py
@@ -1,3 +1,26 @@
+import os
+
+
+def search_file(file_name: str):
+    file_name = file_name.lower().split(" ")
+    file_found = False
+    file_names = []
+
+    if os.path.exists("data"):
+        available_files = os.listdir("data")
+        for file in available_files:
+            file_tokens = file.lower().split(" ")
+            print(file_tokens)
+            for token in file_name:
+                if token not in file_tokens:
+                    break
+            else:
+                file_found = True
+                file_names.append(file)
+
+    return file_found, " ".join(file_names)
+
+
 def process_request(request_tokens):
     processed_request = {}
     if request_tokens[1] == "JOIN":
@@ -12,11 +35,7 @@ def process_request(request_tokens):
         processed_request['request'] = "SER"
         hop_count = int(request_tokens[5]) - 1
 
-        file_found = False
-        file_names = " "
-        ''' TODO
-        Search for the file
-        '''
+        file_found, file_names = search_file(request_tokens[4])
         processed_request['file_found'] = file_found
 
         if file_found:
